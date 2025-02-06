200d790f0cc6309312e7d6a4db39431cc776a9c7
Makksaw <maxim.razumovsky@icloud.com>
2025-02-06 13:05:30 +0200

    Test 1.1

--- server.js
+++ server.js
@@ -10,7 +10,4 @@ app.use(express.static('public'));
 
 app.use(router);
 
-if (require.main === module)
-    app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-module.exports = app;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
