d22d265f50d99de5d4e5e2bf751bf482df17e7b5
Makksaw <maxim.razumovsky@icloud.com>
2025-02-08 13:16:57 +0200

    Delete domain, small changes in server file

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = require('./routes');
-const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.use(router);
 
